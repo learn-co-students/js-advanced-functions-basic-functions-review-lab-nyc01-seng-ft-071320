540fa78ee84cdfec9d6b40c09eac62efe9e28adf
junko911 <junkotahara911@gmail.com>
2020-09-03 22:12:54 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Your code here
+function saturdayFun(activity='roller-skate') {
+  return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function(activity='go to the office') {
+  return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(flair='*') {
+  return function(adj='special') {
+    return `You are ${flair}${adj}${flair}!`
+  }
+}
+
+const Calculator = {
+  add(a, b) {
+    return a + b
+  },
+  subtract(a, b) {
+    return a - b
+  },
+  multiply(a, b) {
+    return a * b
+  },
+  divide(a, b) {
+    return a / b
+  }
+}
+
+function actionApplyer(int, functions) {
+  if (functions.length === 0) { return int }
+  let a = int
+  functions.forEach(func => a = func(a))
+  return a
+}
